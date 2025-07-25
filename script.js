ecb3fb9897be564412905b78b888c8a646e6ba67
Yoga Vignesh V <121656366+yogaV28@users.noreply.github.com>
2025-07-26 00:36:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+ScrollReveal().reveal('.title', {
+  origin: 'top',
+  distance: '50px',
+  duration: 1000,
+  delay: 200,
+  reset: false
+});
+
+ScrollReveal().reveal('.subtitle', {
+  origin: 'bottom',
+  distance: '30px',
+  duration: 1000,
+  delay: 400,
+  reset: false
+});
+
+ScrollReveal().reveal('.button-group button', {
+  interval: 200,
+  origin: 'bottom',
+  distance: '20px',
+  duration: 1000,
+  reset: false
+});
